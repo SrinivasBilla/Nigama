3978a57e2c34eff9d01e43c04070f1e684eae9f6
Billa Srinivas <srinivasbilla@IICAV.AD>
2024-01-08 08:05:27 -0500

    first commit

---
+++ wp-content/themes/nigama/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main Template file
+ * 
+ * @package Nigama
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nigama Foundation</title>
+</head>
+<body>
+    <h1>text</h1>
+</body>
+</html>
